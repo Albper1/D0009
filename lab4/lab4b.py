ee5e10bf0cd67b9a12658c69fd98a4928ddab1ac
Albin Persson <albpe050@student.liu.se>
2021-09-23 01:06:42 +0200

    kanske klar

--- lab4/lab4b.py
+++ lab4/lab4b.py
@@ -8,7 +8,6 @@ class Phonebook:
     def main(self):
         while(True):
             l = input('phonebook> ').split()
-            print(l)
             if l[0] == 'add':
                 self.add(l[1],l[2])
             elif l[0] == 'lookup':
@@ -49,18 +48,29 @@ class Phonebook:
     '''saves a file as filename, either overwrite a file or create a new one'''
     def save(self,filename):
         f = open(filename, "w")
-        f.write(json.dumps(self.dic, indent = 4))
-        f.write(json.dumps(self.a_dic, indent = 4))
+        res = ""
+        for i in self.dic:
+            res +=f"{i};{self.dic[i]}\n"
+        if self.a_dic:
+            res +="alias_dic\n"
+            for i in self.a_dic:
+                res +=f"{i};{self.a_dic[i]}\n"
+
+        f.write(res)
         f.close()
     '''load the file "filename" if there is one'''
     def load(self,filename):
         try:
             with open(filename,'r') as f:
-                data = f.read()
-            self.dic,self.a_dic = data.split(";")
-            #print(test)
-            print(self.dic)
-            #print(self.a_dic)
+                lines = f.readlines()
+            dic_lines = lines[:lines.index('alias_dic\n')]
+            a_dic_lines = lines[lines.index('alias_dic\n')+1:]
+            for line in dic_lines:
+                name,nr = line.split(';')
+                self.dic[name] = nr.strip()
+            for line in a_dic_lines:
+                name,alias_lst = line.split(';')
+                self.a_dic[name] = alias_lst.strip()
 
             f.close()
         except FileNotFoundError:
@@ -69,6 +79,4 @@ class Phonebook:
     def quit(self):
         quit()
 
-p = Phonebook().main(
-
-)
+p = Phonebook().main()
